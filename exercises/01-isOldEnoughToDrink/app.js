2fa843a0741ac86db4530f8af126b2ac43101f5b
tebanban <estebancalf@gmail.com>
2021-05-12 16:51:18 +0000

    exc 2

--- exercises/01-isOldEnoughToDrink/app.js
+++ exercises/01-isOldEnoughToDrink/app.js
@@ -1,3 +1,9 @@
-function isOldEnoughToDrink(){
+function isOldEnoughToDrink(age){
     // your code here	
-}
+    return age >= 21
+        
+    }
+
+
+var output = isOldEnoughToDrink(22);
+console.log(output)
