3bd61cb7627f40f4660a14bbde2462879f51cc5d
tebanban <estebancalf@gmail.com>
2021-05-13 19:09:45 +0000

    exc 12

--- exercises/08-checkAge/app.js
+++ exercises/08-checkAge/app.js
@@ -1,3 +1,13 @@
 function checkAge(name, age) {
   // your code here
+  if (age<21){
+      message="Go home, " + name +"!"
+  }
+
+  else {message="Welcome, " +name +"!"}
+      
+  return message
 }
+
+var output = checkAge('Adrian', 22);
+console.log(output); // --> 'Welcome, Adrian!'S
--- exercises/11-getLengthOfTwoWords/app.js
+++ exercises/11-getLengthOfTwoWords/app.js
@@ -1,3 +1,6 @@
 function getLengthOfTwoWords(word1, word2) {
   // your code here
+  return (word1.length + word2.length )
 }
+var output = getLengthOfTwoWords('some', 'words');
+console.log(output); // --> 9
