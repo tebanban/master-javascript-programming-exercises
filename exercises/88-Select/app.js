ac4be42cf03c456004602716c7289cbb5df88372
tebanban <estebancalf@gmail.com>
2021-05-18 04:32:36 +0000

    exc 90

--- exercises/88-Select/app.js
+++ exercises/88-Select/app.js
@@ -1,1 +1,26 @@
 // Write your function here
+
+function select (arr, obj){
+
+var newObj= {};
+for (var key in obj) {
+    for (var i = 0; i < arr.length; i++) {
+       if (arr[i] === key) {
+           newObj[key] = obj[key]
+            
+   }
+  }
+}
+return newObj
+}
+
+
+var arr = ['a', 'c', 'e'];
+var obj = {
+  a: 1,
+  b: 2,
+  c: 3,
+  d: 4
+};
+var output = select(arr, obj);
+console.log(output); // --> { a: 1, c:3 }
--- exercises/89-getElements/app.js
+++ exercises/89-getElements/app.js
@@ -1,1 +1,24 @@
 // Write your function here
+
+function getElementsThatEqual10AtProperty(obj, key){
+    
+var array= [];
+ for( var key in obj){
+     for(let i=0; i < obj[key].length; i++) {
+         if(obj[key][i] === 10){
+             array.push(obj[key][i])
+         }
+     }
+      return array
+ }
+}
+
+ 
+    
+
+
+var obj = {
+  key: [1000, 10, 50, 10]
+};
+var output = getElementsThatEqual10AtProperty(obj, 'key');
+console.log(output); // --> [10, 10]
